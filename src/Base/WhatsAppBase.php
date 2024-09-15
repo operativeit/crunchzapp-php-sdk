92c345993f84e5dce437c1aa1f93af3bf3cdf4b7
Rafi Halilintar <masitingss@gmail.com>
2024-09-15 14:14:06 +0700

    Added new traits and methods to WhatsAppBase
    - Introduced ChatTrait and GroupTrait in WhatsAppBase class
    - Added checkPhoneNumber and health methods in WhatsAppBase
    - Created new files for ChatTrait and GroupTrait with respective functionalities
    - Removed checkNumber method from MessageTrait

--- src/Base/WhatsAppBase.php
+++ src/Base/WhatsAppBase.php
@@ -2,7 +2,9 @@
 
 namespace CrunchzApp\Base;
 
+use Crunchzapp\Traits\ChatTrait;
 use Crunchzapp\Traits\ContactTrait;
+use Crunchzapp\Traits\GroupTrait;
 use Crunchzapp\Traits\MessageTrait;
 
 class WhatsAppBase {
@@ -13,7 +15,7 @@ class WhatsAppBase {
     protected $seen = false;
     protected $endpoint = 'https://api.crunchz.app/api';
 
-    use MessageTrait, ContactTrait;
+    use MessageTrait, ContactTrait, ChatTrait, GroupTrait;
 
     public function contact($contactId): static
     {
@@ -21,4 +23,39 @@ class WhatsAppBase {
         return $this;
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function checkPhoneNumber($phoneNumber, $toVariable = false)
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        $client = $this->client->withToken($this->token)->get('channel/check-phone-number', [
+            'phone' => $phoneNumber
+        ]);
+        if ($toVariable) {
+            $data = $client->json();
+            if ($data['data']['is_exists']) {
+                $this->contactId = $data['data']['contact_id'];
+                return $this;
+            } else {
+                throw new \Exception('The phone number is not exists on WhatsApp');
+            }
+        } else {
+            return $client->json();
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function health()
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        $client = $this->client->withToken($this->token)->get('channel/health');
+        return $client->json();
+    }
 }
---
+++ src/Traits/ChatTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Crunchzapp\Traits;
+
+trait ChatTrait {
+
+    /**
+     * @throws \Exception
+     */
+    public function allChat($limit = 10, $offset = 0)
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        $client = $this->client->withToken($this->token)->get('chat', [
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+        return $client->json();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function chatDetail()
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        if (is_null($this->contactId)) {
+            throw new \Exception('Contact Id is required');
+        }
+        $client = $this->client->withToken($this->token)->get('chat/detail', [
+            'contact_id' => $this->contactId
+        ]);
+        return $client->json();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function archiveChat()
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        if (is_null($this->contactId)) {
+            throw new \Exception('Contact Id is required');
+        }
+        $client = $this->client->withToken($this->token)->post('chat/archive', [
+            'contact_id' => $this->contactId
+        ]);
+        return $client->json();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function unArchiveChat()
+    {
+        if (is_null($this->token)) {
+            throw new \Exception('Channel token is required');
+        }
+        if (is_null($this->contactId)) {
+            throw new \Exception('Contact Id is required');
+        }
+        $client = $this->client->withToken($this->token)->post('chat/un-archive', [
+            'contact_id' => $this->contactId
+        ]);
+        return $client->json();
+    }
+}
